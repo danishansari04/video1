1ed4bab648b452122a61e26d9bb697457ce23e5a
Danish Ansari <117536349+danishansari04@users.noreply.github.com>
2024-03-14 10:24:00 +0530

    Update danish.js

--- danish.js
+++ danish.js
@@ -1,7 +1,7 @@
 while (true) {
     var password = prompt("Please enter the password:");
 
-    if (password === "11") {
+    if (password === "danish") {
         alert("Access granted!");
         break; // Exit the loop if the password is correct
     } else {
